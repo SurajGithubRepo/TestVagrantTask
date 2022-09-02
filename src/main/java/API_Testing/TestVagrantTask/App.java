a19cca7f2fb24cdf467b2cc28b5ce5418dd63c2b
SURAJ <SURAJ@DESKTOP-SMQL3V0>
2022-09-02 10:10:36 +0530

    [Suraj Deshpande]This task is completed by Suraj

---
+++ src/main/java/API_Testing/TestVagrantTask/App.java
@@ -0,0 +1,13 @@
+package API_Testing.TestVagrantTask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
